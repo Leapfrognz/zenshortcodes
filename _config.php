ca085f54475bddae09f102f6235d3467acf1b294
James Ayers <james.ayers@dna.co.nz>
2013-12-05 15:40:06 +1300

    add to previously defined valid elements, not overide

--- _config.php
+++ _config.php
@@ -8,7 +8,7 @@ object::useCustomClass('HtmlEditorField_Toolbar', 'ZenHtmlEditorField_Toolbar');
 $config = HtmlEditorConfig::get('cms');
 $config->enablePlugins(array('zenshortcodes' => '/'.ZENSHORTCODES_PATH.'/editor_plugin_src.js'));
 $config->addButtonsToLine(2, 'zenshortcodes');	
-$config->setOption('valid_elements', 'a[href|target|rel|class],b,br,font,img[src|id|width|height|align|hspace|vspace,KeepThis,true,TB_iframe],i,li,p,h1,h2,h3,h4,h5,h6,span[class],textformat[blockindent|indent|leading|leftmargin|rightmargin|tabstops],u,table,tr,td,*[contenteditable|class],p[class]');
 $config->setOption('content_css', '/'.ZENSHORTCODES_PATH.'/css/zenshortcodes.css');
+$config->setOption('extended_valid_elements', '*[contenteditable|class|id]');
 
 ShortcodeParser::get('default')->register('zenshortcode', array('ZenShortCode', 'ShortcodeHandler'));
